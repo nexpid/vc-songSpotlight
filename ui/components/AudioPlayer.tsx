49762368f1ae740374df794fac21a364f5dda0f6
nexpid <me@nexpid.xyz>
2026-07-07 21:25:26 +0200

    fix: add list to useCallback deps

--- ui/components/AudioPlayer.tsx
+++ ui/components/AudioPlayer.tsx
@@ -107,7 +107,7 @@ export default function AudioPlayer({ audioRef, list, playing, setPlaying, setLo
             }
             audios.current.delete(index);
         }
-    }, []);
+    }, [list]);
 
     const handleLoaded = useCallback((index: number) => {
         loaded.current.add(index);
